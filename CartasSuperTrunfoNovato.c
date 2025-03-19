4df7595283e2c318d80027febd8feef16cc82482
Fernando <alves.sup@gmail.com>
2025-03-18 23:13:47 -0300

    Update CartasSuperTrunfoNovato.c

--- CartasSuperTrunfoNovato.c
+++ CartasSuperTrunfoNovato.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
+// Desafio Super Trunfo - Estados
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
 
 int main() {
     //Váriaveis dos dados das duas cartas
